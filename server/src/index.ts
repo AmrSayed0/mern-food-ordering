bb2e6c700fdba5d9be1d7595cbbff9cd09318ed8
AmrSayed0 <amracademycode@gmail.com>
2024-05-30 18:44:50 +0300

    Create restaurant API

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,8 +1,16 @@
-import express from "express";
+import express, { Request, Response } from "express";
 import "dotenv/config";
 import cors from "cors";
 import mongoose from "mongoose";
 import myUserRoute from "./routes/myUserRoute";
+import myRestaurantRoute from "./routes/MyRestaurantRoute";
+import { v2 as cloudinary } from "cloudinary";
+
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
 
 mongoose.connect(process.env.MONGODB_CONNECTION_STRING as string).then(() => {
   console.log("Database connected successfully");
@@ -12,7 +20,12 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
+app.get("/health", async (req: Request, res: Response) => {
+  res.send({ message: "health OK!" });
+});
+
 app.use("/api/my/user", myUserRoute);
+app.use("/api/my/restaurant", myRestaurantRoute);
 
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
